dbd512889a1db939f07cd9fdffa16880188726da
AnimeMnaji <154734036+Animemanji@users.noreply.github.com>
2025-01-17 23:04:23 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import telebot
 
-API_TOKEN = '6139882386:AAFZUzoNQTYoGsszz4TQbZEuB4aH2ZxgeaQ'
+API_TOKEN = ''
 bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
 
